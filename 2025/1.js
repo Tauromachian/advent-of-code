56f0818fae53188dae520f93a78bc3cf47b2d004
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-09-04 21:35:23 +0200

    Fix aoc 2025 1

--- 2025/1.js
+++ 2025/1.js
@@ -1,11 +1,13 @@
+const fs = require("node:fs");
+
+let input = fs.readFileSync("./1.input.txt", { encoding: "utf-8" });
+input = input.split("\n");
+
 function rotateLeft(barrel, amount) {
   const result = barrel - amount;
 
-  if (result < 0) {
-    return 100 + result;
-  }
-
   if (result === 100) return 0;
+  if (result < 0) return 100 + result;
 
   return result;
 }
@@ -13,36 +15,35 @@ function rotateLeft(barrel, amount) {
 function rotateRight(barrel, amount) {
   const result = barrel + amount;
 
-  if (result > 100) {
-    return result - 100;
-  }
-
   if (result === 100) return 0;
+  if (result > 100) return result - 100;
 
   return result;
+}
+
+function ensureTwoDigits(value) {
+  if (value > 99) return value % 100;
+
+  return value;
 }
 
 (function start() {
   let password = 0;
   let barrel = 50;
 
-  process.argv.forEach((arg) => {
-    if (arg.includes("L")) {
-      const number = Number(arg.replace("L", ""));
-
-      barrel = rotateLeft(barrel, number);
+  for (const item of input) {
+    if (item.includes("L")) {
+      const number = Number(item.replace("L", ""));
+      barrel = rotateLeft(barrel, ensureTwoDigits(number));
     }
 
-    if (arg.includes("R")) {
-      const number = Number(arg.replace("R", ""));
-
-      barrel = rotateRight(barrel, number);
+    if (item.includes("R")) {
+      const number = Number(item.replace("R", ""));
+      barrel = rotateRight(barrel, ensureTwoDigits(number));
     }
 
-    if (barrel == 0) {
-      password++;
-    }
-  });
+    if (barrel == 0) password++;
+  }
 
   console.log(password);
 })();
