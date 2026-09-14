8fde0eb662570cf48a7f51dd1e1f1dc2e420ee1b
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-09-14 20:21:42 +0200

    Complete 2025/2.2

---
+++ 2025/2.2.js
@@ -0,0 +1,42 @@
+const fs = require("node:fs");
+
+let input = fs.readFileSync("./input.txt", { encoding: "utf-8" });
+input = input.split(",");
+
+let total = 0;
+
+function hasRepeatedPatterns(value) {
+  const string = String(value);
+  const stopNumber = Math.floor(string.length / 2);
+
+  let pattern = "";
+  while (pattern.length < stopNumber) {
+    pattern = string.slice(0, pattern.length + 1);
+    const regex = new RegExp(pattern, "g");
+
+    const arrayMatch = string.match(regex);
+    if (arrayMatch.length * pattern.length === string.length) return true;
+  }
+
+  return false;
+}
+
+input.forEach((range) => {
+  const rangeArray = range.split("-");
+
+  let stringStart = String(rangeArray[0]);
+
+  let start = Number(stringStart);
+  const end = Number(rangeArray[1]);
+
+  while (start <= end) {
+    const hasPatterns = hasRepeatedPatterns(stringStart);
+
+    if (hasPatterns) total += start;
+
+    start++;
+    stringStart = String(start);
+  }
+});
+
+console.log(total);
